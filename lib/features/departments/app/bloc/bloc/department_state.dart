8b980f8f18bc327c8d9fea4eb90a80af25e84ca3
adolesamuel <adolesamuel4@gmail.com>
2021-04-29 17:23:42 +0100

    Department: Bloc Model created

---
+++ lib/features/departments/app/bloc/bloc/department_state.dart
@@ -0,0 +1,24 @@
+part of 'department_bloc.dart';
+
+abstract class DepartmentState extends Equatable {
+  const DepartmentState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class DepartmentInitial extends DepartmentState {}
+
+class DepartmentLoadingState extends DepartmentState {}
+
+class DepartmentErrorState extends DepartmentState {
+  final Failure failure;
+
+  DepartmentErrorState(this.failure);
+}
+
+class ListOfDepartmentState extends DepartmentState {
+  final List<DeptEntity> listOfDept;
+
+  ListOfDepartmentState(this.listOfDept);
+}
