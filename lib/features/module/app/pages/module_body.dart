4d92f32815dd38e2e927531611a1b40a30708fa8
adolesamuel <adolesamuel4@gmail.com>
2021-05-07 17:52:22 +0100

    In progress

--- lib/features/module/app/pages/module_body.dart
+++ lib/features/module/app/pages/module_body.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:wowca_task/features/module/app/widgets/module_container.dart';
+import 'package:wowca_task/features/module/domain/entity/module_entity.dart';
 
 class ModuleBody extends StatefulWidget {
+  final ModuleEntity module;
+
+  const ModuleBody({Key key, this.module}) : super(key: key);
   @override
   _ModuleBodyState createState() => _ModuleBodyState();
 }
 
 class _ModuleBodyState extends State<ModuleBody> {
+  TextEditingController moduleSearchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        TextField(
+          controller: moduleSearchController,
+          decoration: InputDecoration(
+            labelText: 'Search for Modules',
+            suffixIcon: Icon(Icons.search),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    ModuleContainer(),
+                  ],
+                );
+              }),
+        ),
+      ],
+    );
+    ;
   }
 }
