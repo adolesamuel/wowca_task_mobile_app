ccce44eeec7fdfa224b922be883b1b463673175b
adolesamuel <adolesamuel4@gmail.com>
2021-05-20 15:59:06 +0100

    Department: domain layer okay

--- lib/features/departments/domain/usecases/create_dept.dart
+++ lib/features/departments/domain/usecases/create_dept.dart
@@ -2,17 +2,16 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:wowca_task/core/failures/failure.dart';
 import 'package:wowca_task/core/usecase/usecase.dart';
-import 'package:wowca_task/features/departments/domain/entity/created_dept_entity.dart';
+import 'package:wowca_task/features/departments/domain/entity/department_entity.dart';
 import 'package:wowca_task/features/departments/domain/repository/dept_repository.dart';
 
-class CreateDept extends Usecase<CreatedDeptEntity, CreateDeptParams> {
+class CreateDept extends Usecase<DeptEntity, CreateDeptParams> {
   final DepartmentRepository departmentRepository;
 
   CreateDept(this.departmentRepository);
 
   @override
-  Future<Either<Failure, CreatedDeptEntity>> call(
-      CreateDeptParams params) async {
+  Future<Either<Failure, DeptEntity>> call(CreateDeptParams params) async {
     return await departmentRepository.createDept(
       deptDescription: params.departmentDescription,
       deptName: params.departmentName,
