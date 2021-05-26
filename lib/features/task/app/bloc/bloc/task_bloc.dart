c520bee1dbbb35e875f77ddb4151e3601305a548
adolesamuel <adolesamuel4@gmail.com>
2021-05-26 13:03:30 +0100

    Task values updated in injection container

--- lib/features/task/app/bloc/bloc/task_bloc.dart
+++ lib/features/task/app/bloc/bloc/task_bloc.dart
@@ -2,12 +2,38 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:wowca_task/features/task/domain/usecases/create_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/delete_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/finish_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/get_tasks.dart';
+import 'package:wowca_task/features/task/domain/usecases/reset_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/start_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/suspend_task.dart';
+import 'package:wowca_task/features/task/domain/usecases/update_task.dart';
 
 part 'task_event.dart';
 part 'task_state.dart';
 
 class TaskBloc extends Bloc<TaskEvent, TaskState> {
-  TaskBloc() : super(TaskInitial());
+  final CreateTask createTask;
+  final DeleteTask deleteTask;
+  final FinishTask finishTask;
+  final GetTasks getTasks;
+  final ResetTask resetTask;
+  final StartTask startTask;
+  final SuspendTask suspendTask;
+  final UpdateTask updateTask;
+
+  TaskBloc({
+    this.createTask,
+    this.deleteTask,
+    this.finishTask,
+    this.getTasks,
+    this.resetTask,
+    this.startTask,
+    this.suspendTask,
+    this.updateTask,
+  }) : super(TaskInitial());
 
   @override
   Stream<TaskState> mapEventToState(
