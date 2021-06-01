a284d07d1ac52761ce1049bbdaee1403c320c1ac
adolesamuel <adolesamuel4@gmail.com>
2021-06-01 14:14:40 +0100

    Update module_body.dart

--- lib/features/module/app/pages/module_body.dart
+++ lib/features/module/app/pages/module_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wowca_task/features/module/app/bloc/module_bloc.dart';
 import 'package:wowca_task/features/module/app/widgets/module_container.dart';
 import 'package:wowca_task/features/module/domain/entity/module_entity.dart';
+import 'package:wowca_task/injection_container.dart';
 
 class ModuleBody extends StatefulWidget {
   final ModuleEntity module;
@@ -11,7 +14,20 @@ class ModuleBody extends StatefulWidget {
 }
 
 class _ModuleBodyState extends State<ModuleBody> {
+  final moduleBloc = sl<ModuleBloc>();
+
   TextEditingController moduleSearchController = TextEditingController();
+
+  BlocProvider<ModuleBloc> buildModuleList(BuildContext context) {
+    ///Lay out loading views for when
+    ///module is being loaded for the first time
+    final loaderView = Center(
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: [],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +42,26 @@ class _ModuleBodyState extends State<ModuleBody> {
           ),
         ),
         Expanded(
-          child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    ModuleContainer(),
-                  ],
-                );
-              }),
+          child: BlocProvider(
+            create: (context) => moduleBloc..add(GetModulesEvent()),
+            child: BlocBuilder<ModuleBloc, ModuleState>(
+              builder: (context, state) {
+                if (state is ListOfModulesReceivedState) {
+                  return ListView.builder(
+                      itemCount: state.modules.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            ModuleContainer(),
+                          ],
+                        );
+                      });
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+          ),
         ),
       ],
     );
